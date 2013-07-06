56cd9c1ef390258c5436885156820e145f92dfd4
charekcaj <charekcaj@gmail.com>
2013-07-06 05:54:20 +0000

    Change web.js with buf.toString and utf-8 encoding 5

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(256);
-  response.send(buf.toString('utf-8', fs.readFileSync("index.html")));
+  var buf = new Buffer(fs.readFileSync("index.html"));
+  response.send(buf.toString('utf-8'));
 //  response.send('Hello World 2!');
 });
 
